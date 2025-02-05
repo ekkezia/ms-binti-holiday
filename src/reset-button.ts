e7f1b98e652bc44b2f3f0049c6c5673f7a465a21
Elizabeth Kezia Widjaja <e.kezia@gmail.com>
2025-02-05 16:55:17 +0700

    chore: add original image information for resetting

--- src/reset-button.ts
+++ src/reset-button.ts
@@ -28,7 +28,7 @@ export function generateResetButton(layer: Layer): void {
       const dataToBeReset = data?.filter((item: { ip_address: null; }) => item.ip_address !== null) || [];
 
       // Fetch original image data locally
-      const response = await fetch('/data/images.json');
+      const response = await fetch('https://lmgbcuolwhkqoowxnaik.supabase.co/storage/v1/object/public/ms-binti-holiday//images.json');
       const imagesData: ImageData[] = await response.json();
 
       for (let i = 0; i < dataToBeReset.length; i++) {
